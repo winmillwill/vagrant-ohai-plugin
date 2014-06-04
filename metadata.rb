1b82dc9ad5b93c16856439b1b8300b4cfc966791
Will Milton <wa.milton@gmail.com>
2014-06-03 20:16:34 -0500

    Bumping

--- metadata.rb
+++ metadata.rb
@@ -4,20 +4,4 @@ maintainer_email "tpbrown@gmail.com"
 license          "Apache 2.0"
 description      "Ohai plugin that detects Vagrant"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
-
-recipe "vagrant-ohai-plugin::default", "Installs the Vagrant ohai plugin"
-
-attribute "ohai/plugin_path",
-  :display_name => "Ohai Plugin Path",
-  :description => "Distribute plugins to this path.",
-  :type => "string",
-  :required => "optional",
-  :default => "/etc/chef/ohai_plugins"
-
-attribute "ohai/plugins",
-  :display_name => "Ohai Plugin Sources",
-  :description => "Read plugins from these cookbooks and paths",
-  :type => "hash",
-  :required => "optional",
-  :default => {'ohai' => 'plugins'}
+version          "0.1.2"
