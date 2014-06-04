10b40e455433481d00a43b2dd2f7fac2608f5c8b
Will Milton <wa.milton@gmail.com>
2014-06-04 00:11:15 -0500

    can I even copy-paste correctly?

--- files/default/vagrant/vagrant-net.rb
+++ files/default/vagrant/vagrant-net.rb
@@ -1,12 +1,14 @@
-provide "ipaddress"
-depends "ipaddress", "network/interfaces", "etc/passwd"
+Ohai.plugin(:Vagrantipaddress) do
+  provides "ipaddress"
+  depends "ipaddress", "network/interfaces", "etc/passwd"
 
-collect_data(:default) do
-  if etc["passwd"].any? { |k,v| k == "vagrant"}
-    if network["interfaces"]["eth1"]
-      network["interfaces"]["eth1"]["addresses"].each do |ip, params|
-        if params['family'] == ('inet')
-          ipaddress ip
+  collect_data(:default) do
+    if etc["passwd"].any? { |k,v| k == "vagrant"}
+      if network["interfaces"]["eth1"]
+        network["interfaces"]["eth1"]["addresses"].each do |ip, params|
+          if params['family'] == ('inet')
+            ipaddress ip
+          end
         end
       end
     end
